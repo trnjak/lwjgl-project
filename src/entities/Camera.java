12302dd651e98f97465e7048d76860e80e0c11fe
Marko <110738528+trnjak@users.noreply.github.com>
2024-03-16 12:17:23 +0100

    Initial commit.

---
+++ src/entities/Camera.java
@@ -0,0 +1,38 @@
+package entities;
+
+import org.lwjgl.input.*;
+import org.lwjgl.util.vector.*;
+
+public class Camera {
+	private Vector3f pos = new Vector3f(0,0,0);
+	private float pitch;
+	private float yaw;
+	private float roll;
+	public Camera(){}
+	public void move(){
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
+			pos.z-=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+			pos.z+=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
+			pos.x+=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
+			pos.x-=0.02f;
+		}
+	}
+	public Vector3f getPos() {
+		return pos;
+	}
+	public float getPitch() {
+		return pitch;
+	}
+	public float getYaw() {
+		return yaw;
+	}
+	public float getRoll() {
+		return roll;
+	}
+}
